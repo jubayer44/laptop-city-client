78dcf03a3c1308f94ac6940e66184e6b312508e7
jubayer44 <jubayerahmedshamim44@gmail.com>
2022-11-24 13:09:36 +0600

    get-categories-data-from-server

--- src/Pages/Home/Categories/Categories.js
+++ src/Pages/Home/Categories/Categories.js
@@ -1,39 +1,50 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Spinner from "../../../components/Spinner";
 
 const Categories = () => {
   const [categories, setCategories] = useState([]);
+  const [loader, setLoader] = useState(true);
 
   useEffect(()=> {
-    fetch('categories.json')
+    fetch(`${process.env.REACT_APP_URL}/categories`)
     .then(res => res.json())
     .then(data => {
-      setCategories(data)
+      setCategories(data);
+      setLoader(false);
     })
-  }, [])
-  
-console.log(categories);
-    return (
-        <div>
-      <h2 className="text-2xl mt-8 font-bold text-center">Explore By Category</h2>
+    .catch(err => console.log(err));
+  })
+
+    if(loader){
+      return <Spinner/>
+    }
+
+  return (
+    <div>
+      <h2 className="text-2xl mt-8 font-bold text-center">
+        Explore By Category
+      </h2>
       <div className=" py-8 mx-auto  lg:py-8">
         <div className="grid gap-5 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 sm:mx-auto ">
-          {
-            categories?.map(category => (<div className="overflow-hidden transition-shadow duration-300 bg-white rounded max-w-xs">
-            <Link to="/category" aria-label="Article">
-              <img
-                src={category?.img}
-                className="object-cover w-full rounded h-[200px]"
-                alt=""
-              />
-              <h3 className='text-2xl text-center'>{category?.CategoryName} brand</h3>
-            </Link>
-          </div>))
-          }
+          {categories?.map((category) => (
+            <div key={category._id} className="overflow-hidden transition-shadow duration-300 bg-white rounded max-w-xs">
+              <Link to={`/category/${category._id}`} aria-label="Article">
+                <img
+                  src={category?.img}
+                  className="object-cover w-full rounded h-[200px]"
+                  alt=""
+                />
+                <h3 className="text-2xl text-center">
+                  {category?.CategoryName} brand
+                </h3>
+              </Link>
+            </div>
+          ))}
         </div>
       </div>
     </div>
-    );
+  );
 };
 
 export default Categories;
